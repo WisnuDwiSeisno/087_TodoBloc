a844212d05fc8a7537d6e5401109278d7d966054
WisnuDwiSeisno <wseisno37@gmail.com>
2025-05-08 21:51:14 +0700

    memanggil ke main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_beta/presentation/todo_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TodoPage(),
     );
   }
 }
