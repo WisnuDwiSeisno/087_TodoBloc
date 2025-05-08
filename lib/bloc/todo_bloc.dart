089afbedbdf8820b815017cbf686850315a239de
WisnuDwiSeisno <wseisno37@gmail.com>
2025-05-08 18:44:56 +0700

    Menambahkan aksi toggle status selesai pada todo

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -28,5 +28,25 @@ class TodoBloc extends Bloc<TodoEvent, TodoState> {
         emit(TodoLoaded(todos: currentState.todos, selectedDate: event.date));
       }
     });
+    
+    on<TodoEventComplete>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        final List<Todo> updatedTodos = List.from(currentState.todos);
+        if (event.index >= 0 && event.index < updatedTodos.length) {
+          updatedTodos[event.index] = Todo(
+            title: updatedTodos[event.index].title,
+            date: updatedTodos[event.index].date,
+            isCompleted: !updatedTodos[event.index].isCompleted,
+          );
+        }
+        emit(
+          TodoLoaded(
+            todos: updatedTodos,
+            selectedDate: currentState.selectedDate,
+          ),
+        );
+      }
+    });
   }
 }
