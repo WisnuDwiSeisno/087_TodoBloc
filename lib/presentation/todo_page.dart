e96276e60b53da0d9e3da7d19d8ffca21a6f9da0
WisnuDwiSeisno <wseisno37@gmail.com>
2025-05-08 19:10:02 +0700

    Menambahkan form input untuk menambahkan todo baru

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -36,24 +36,65 @@ class TodoPage extends StatelessWidget {
                     ],
                   ),
                   SizedBox(width: 16.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime(2100),
-                      ).then((selectedDate) {
-                        if (selectedDate != null) {
-                          context.read<TodoBloc>().add(
-                            TodoSelectedDate(date: selectedDate),
-                          );
-                        }
-                      });
-                    },
-                    child: Text('Select Date'),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        ).then((selectedDate) {
+                          if (selectedDate != null) {
+                            context.read<TodoBloc>().add(
+                              TodoSelectedDate(date: selectedDate),
+                            );
+                          }
+                        });
+                      },
+                      child: Text('Select Date'),
+                    ),
                   ),
                 ],
+              ),
+              Form(
+                key: _key,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          labelText: 'Todo',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please enter a todo";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    FilledButton(
+                      onPressed: () {
+                        if (_key.currentState!.validate()) {
+                          final selectedDate = context.read<TodoBloc>().state;
+                          if (selectedDate is TodoLoaded) {
+                            context.read<TodoBloc>().add(
+                              TodoEventAdd(
+                                title: _controller.text,
+                                date: selectedDate.selectedDate!,
+                              ),
+                            );
+                            _controller.clear();
+                          }
+                        }
+                      },
+                      child: Text('Tambah'),
+                    ),
+                  ],
+                ),
               ),
               SizedBox(height: 16.0),
               Expanded(
