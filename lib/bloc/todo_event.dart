59a32f974bf9e37c96a7087d7e0bb97bf33710ee
WisnuDwiSeisno <wseisno37@gmail.com>
2025-05-08 18:46:44 +0700

    enambahkan event untuk menandai todo sebagai selesai atau belum

--- lib/bloc/todo_event.dart
+++ lib/bloc/todo_event.dart
@@ -8,3 +8,7 @@ final class TodoEventAdd extends TodoEvent {
   TodoEventAdd({required this.title, required this.date});
 }
 
+final class TodoEventComplete extends TodoEvent {
+  final int index;
+  TodoEventComplete({required this.index});
+}
